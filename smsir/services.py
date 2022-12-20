0341e3f2a813c995cb30b50761b3a8a296182eb2
mojtabaakbari <mojtaba.akbari.221b@gmail.com>
2022-12-20 17:13:27 +0330

    add post send & bulk send sms to class

--- smsir/services.py
+++ smsir/services.py
@@ -1,2 +1,44 @@
+try :
+    import faster_than_requests as requests
+except ModuleNotFoundError:
+    import requests
+
+
 class SmsIr :
-    pass
+    ENDPOINT = 'https://api.sms.ir'
+
+    def __init__(self, api_key, linenumber) -> None:
+        self._linenumber = linenumber
+        self._headers = {
+            "X-API-KEY": api_key,
+            'ACCEPT': 'application/json',
+            'Content-Type': 'application/json',
+        }
+
+    def send_sms(self, number, message, linenumber=None):
+        self.send_bulk_sms(
+            numbers=[number],
+            message=message,
+            linenumber=linenumber,
+        )
+
+    def send_bulk_sms(self, numbers, message, linenumber=None):
+        url = f'{self.ENDPOINT}/v1/send/bulk'
+
+        data = {
+            'lineNumber': linenumber or self._linenumber,
+            'MessageText': message,
+            'Mobiles': numbers,
+        }
+
+        return self.post(
+            url,
+            data,
+        )
+
+    def post(self, url, data):
+        return requests.post(
+            url,
+            headers=self._headers,
+            json=data,
+        )
