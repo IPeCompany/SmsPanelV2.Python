cf3edfdcfde9f6e0b58abcce0e04cbb86c4b886f
mojtabaakbari <mojtaba.akbari.221b@gmail.com>
2023-02-25 19:19:18 +0330

    change package version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="smsir-python",
-    version="1.0.6",
+    version="1.0.7",
     author="Mojtaba Akbari",
     author_email="mojtaba.akbari.221b@gmail.com",
     packages=["sms_ir"],
